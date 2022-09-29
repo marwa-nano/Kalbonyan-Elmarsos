65b4dbed2f368a03416461d8de32f5f226f9dc58
marwa-nano <61974319+marwa-nano@users.noreply.github.com>
2022-09-29 11:37:41 +0200

    Add files via upload

--- 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/For-loop-challenge.py
+++ 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/For-loop-challenge.py
@@ -1,1 +1,14 @@
+# our fruits list
+fruits = [
+    'apples',
+    'bananes',
+    'pears',
+    'watermellons',
+    'grapes',
+    'dates'
+]
+# for loop to print our list 
+print("Our fruit selection")
+for fruit in fruits:
+    print(fruit)
 
