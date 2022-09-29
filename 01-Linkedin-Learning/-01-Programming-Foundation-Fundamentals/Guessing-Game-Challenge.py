878e683fc355af823c8e51aa827b401c520c3fe4
marwa-nano <61974319+marwa-nano@users.noreply.github.com>
2022-09-29 11:06:46 +0200

    Delete Guessing-Game-Challenge.py

--- 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Guessing-Game-Challenge.py
+++
@@ -1,7 +0,0 @@
-Myfav_food = "pizza"
-guess = input("what's my favrite food")
-if guess == Myfav_food:
-    print("so,amazing that's is")
-else:
-    print("yuck,that's not")    
-print("Thanks for playing")    
