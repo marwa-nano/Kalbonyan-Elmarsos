eda3cb47a507916be946b1520f3a9faf6aff1543
marwa-nano <61974319+marwa-nano@users.noreply.github.com>
2022-09-29 23:50:33 +0200

    Add files via upload

---
+++ 02-Udemy/-02-Js-Andrew/Projects/Notes-app/scripts/notes-app.js
@@ -0,0 +1,51 @@
+"use strict";
+
+let notes = getSavedNotes();
+
+// filter notes by user inputes
+const filters = {
+  searchText: "",
+  sortBy: "byEdited",
+};
+
+renderNotes(notes, filters);
+
+document.querySelector("#create-note").addEventListener("click", (e) => {
+  const id = uuidv4();
+  const timestamp = moment().valueOf();
+
+  notes.push({
+    id: id,
+    title: "",
+    body: "",
+    createdAt: timestamp,
+    updatedAt: timestamp,
+  });
+  saveNotes(notes);
+
+  // 1. Setup Link href to include hash with id
+  // 2. Setup the assign call to include hash with id
+
+  location.assign(`/edit.html#${id}`);
+});
+
+document.querySelector("#search-text").addEventListener("input", (e) => {
+  filters.searchText = e.target.value;
+  renderNotes(notes, filters);
+});
+
+document.querySelector("#filter-by").addEventListener("change", (e) => {
+  filters.sortBy = e.target.value;
+  renderNotes(notes, filters);
+});
+
+window.addEventListener("storage", (e) => {
+  if (e.key === "notes") {
+    notes = JSON.parse(e.newValue);
+    renderNotes(notes, filters);
+  }
+});
+
+// 1. Add createdAt and updatedAt to the new notes (store timestamp)
+// 2. Update updatedAt when someone edits a title or body
+// 3. Delete all old notes before testing
