0eb9c4b590e3352830130a4f300e7768bc1fd251
marwa-nano <61974319+marwa-nano@users.noreply.github.com>
2022-09-29 11:04:12 +0200

    Add files via upload

---
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Favorite-Cities-Challenge.py
@@ -0,0 +1,8 @@
+# print name of favorite city
+def favorite_city(name):
+    print("one of my favorite cities is " ,name )
+
+favorite_city("Alex,Egypt")    
+favorite_city("Cairo,Egypt")    
+favorite_city("Qena,Egypt")    
+favorite_city("Ismailia")    
